17e2c5df8e5588b0f9952e7d2ce436fe242117f9
Md Nisar Ahmed <nisar.a@entomo.co>
2025-09-03 20:31:51 +0530

    Clean Dependency tree

--- core/core-config/src/main/java/com/example/core/config/AuditingConfig.java
+++ core/core-config/src/main/java/com/example/core/config/AuditingConfig.java
@@ -1,4 +1,4 @@
-package com.example.core.domain.config;
+package com.example.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
--- core/core-config/src/main/java/com/example/core/config/SpringSecurityAuditorAware.java
+++ core/core-config/src/main/java/com/example/core/config/SpringSecurityAuditorAware.java
@@ -1,4 +1,4 @@
-package com.example.core.domain.config;
+package com.example.core.config;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
